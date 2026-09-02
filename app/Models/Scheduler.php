48ebe67256bbd5210ede7ba8bdef6a4d724a8090
Ali Kemal Tanriverdi <11221504+alikemaltanriverdi@users.noreply.github.com>
2026-09-02 12:35:01 -0400

    Removed the last_response from the activity_logs

--- app/Models/Scheduler.php
+++ app/Models/Scheduler.php
@@ -38,12 +38,12 @@ class Scheduler extends Model
     parent::boot();
     self::saved(function($model){
       if (!app()->runningInConsole()) {
+        //09/02/2026, AKT - Removing the last_response to be recorded in the activity logs//
+          if ($model->last_response) {
+              unset($model->last_response);
+          }
         $orig = $model->getOriginal();
-        // foreach($orig as $attr => $attr_val) {
-        //   if (isset($model->casts[$attr]) && $model->casts[$attr] === 'object') {
-        //     $orig[$attr] = json_decode($attr_val);
-        //   }
-        // }
+
         $activity_log = new ActivityLog([
           'event' => class_basename($model),
           'new' => $model,
